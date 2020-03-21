f81f00fba1dd60866274b79955a5ba2c90fbd096
AqingCyan <aqingcyan@cyanthing.com>
2020-03-21 20:28:52 +0800

    ADD: menu组件基础

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Button, {ButtonSize, ButtonType} from './components/Button/button'
+import Menu from './components/Menu/menu'
+import MenuItem from './components/Menu/menuItem'
 
 const App: React.FC = () => {
   return (
@@ -18,6 +20,18 @@ const App: React.FC = () => {
         <Button style={{ margin: '0 10px' }} btnType={ButtonType.Danger} size={ButtonSize.Small}>Danger Hello</Button>
         <Button style={{ margin: '0 10px' }} btnType={ButtonType.Link} target="_blank" href="http://www.google.com">Google Link</Button>
         <Button style={{ margin: '0 10px' }} btnType={ButtonType.Link} href="http://www.google.com" disabled>Disabled Link</Button>
+        <hr/>
+        <Menu defaultIndex={0} onSelect={(index) => { alert(index) }}>
+          <MenuItem index={0}>
+            cool link0
+          </MenuItem>
+          <MenuItem index={1}>
+            cool link1
+          </MenuItem>
+          <MenuItem index={2}>
+            cool link2
+          </MenuItem>
+        </Menu>
       </header>
     </div>
   )
